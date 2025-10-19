0c1166cf06c52df46d75cb234d58d5145e99b4df
ns8 <renzo.cossa@outlook.com>
2025-10-18 21:36:12 -0500

    deploy.sh -> build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+tsc
+cp src/index.html build/
+cp src/styles.css build/
--- server/https_server.py
+++ server/https_server.py
@@ -8,12 +8,12 @@ def getSSLContext(certfile: str, keyfile: str):
     return context
 
 handler = http.server.SimpleHTTPRequestHandler
+serverAddress = ("localhost", 5050)
 
-serverAddress = ("127.0.0.1", 5050)
 httpd = http.server.HTTPServer(serverAddress, handler)
 
 context = getSSLContext("cert.pem", "key.pem")
 httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 
-print(f"Server started at: https://{serverAddress[0]}:{serverAddress[1]}/src/index.html")
+print(f"Server started at: https://{serverAddress[0]}:{serverAddress[1]}/build/index.html")
 httpd.serve_forever()
